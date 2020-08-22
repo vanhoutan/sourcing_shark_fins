0be386cfc444f6f88414df696d1d167da5141260
Kyle Van Houtan <kvanhoutan@MBA190232.local>
2020-08-21 23:19:58 -0700

    Added all the visualizations for the Biol Lett draft, plus added commentary on dataviz exploratory plots we never used

--- script/plotting.R
+++ script/plotting.R
@@ -1,11 +1,23 @@
-  # plotting single studies
+  
+##########################################################
+## plotting the outputs of the shark fin sourcing model ##
+##########################################################
+  
+
   library(ggplot2)
   library(tidyverse)
   library(maps)
+  library(tidyr)
+  library(scales)
+  library(sf)
+  library(readr)
+  library(dplyr) 
+  library(lwgeom)
+  library(ggjoy)
+  library(ggridges)
   
   
-  
-  ##  Popular ggPlot theme
+  ##Loading a popular ggPlot theme
 
   themeo <- theme_classic()+
     theme(strip.background = element_blank(),
@@ -17,8 +29,10 @@
           legend.title = element_blank(),
           strip.text = element_text(hjust=0) )
   
-  studies_samp <- list.files('./data/binomial_sampling')
   
+## data wrangling the sourcing model outputs
+  
+  studies_samp <- list.files('./data/binomial_sampling')
   stud_df <- NULL
   
   for(f in 1:length(studies_samp)){
@@ -33,55 +47,84 @@
     return((x-min(x)) / (max(x)-min(x)))
   }
   
-  
   stud_df$seizure <- fct_relevel(stud_df$seizure,c("FEITOSA_COI","MBA_COI","STEINKE_COI","FIELDS_COI"))
-  
   
   stud_df <- stud_df %>%
     group_by(seizure) %>%
     mutate(NORMALIZED = normalize(impact))
   
-  
-  
   mapWorld <- map_data('world', wrap=c(-25,335), ylim=c(-55,75))
   
-  stud_map <- stud_df 
   
-  # main map Fig
+## Main result of sourcing analysis
+## output of the Bernouli trials and SDM stacks
+## becomes Figure 2a  
+
+  stud_map <- stud_df 
   dens_maps <- ggplot()+
     geom_raster(data = stud_map, aes(if_else(lon2 > 178, lon2 - 2, lon2),Lat,fill=NORMALIZED), show.legend = T) +
     geom_polygon(data = mapWorld, aes(x=if_else(long > 178, long - 2, long), y = lat, group = group), 
-                 #fill = "#303030", color = "#303030", 
-                 fill = 'black', color = 'black',
-                 size = .1)+
-    #scale_fill_gradientn(colours = c("black","black",'#4c001f','#72002f','#9e0142','#d53e4f','#f46d43','#fdae61','#fee08b','#ffffbf','white','white'),na.value = "black")+
-    
-    # good one
-    #scale_fill_gradientn(colours = rev(c('#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026','black')),na.value = "black")+
-    
+    ## continent and country polygon fill and border 
+          fill = 'black', color = 'black',size = .1)+
+   
     scale_fill_gradientn(colours = rev(c("light yellow",'#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026','black')),na.value = "black")+
+    #scale_fill_gradientn(colours = c("black","black",'#4c001f','#72002f','#9e0142','#d53e4f','#f46d43','#fdae61','#fee08b','#ffffbf','white','white'),na.value = "black")+
+    #scale_fill_gradientn(colours = rev(c('#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026','black')),na.value = "black")+
     #scale_fill_gradientn(colours = c("black",'#4c001f','#72002f','#72002f','#9e0142','#9e0142','#d53e4f','#d53e4f','#f46d43','#f46d43','#fdae61','#fee08b','#ffffbf','white','white'),na.value = "black")+
     #scale_fill_gradientn(colours = c("black","black",'#4c001f','#72002f','#9e0142','#d53e4f','#f46d43','#fdae61','#fee08b','#ffffbf','#e6f598','#abdda4','#66c2a5','#3288bd','#5e4fa2'),na.value = "black")+
     #scale_fill_gradientn(colours = c("black","black","#440154FF", "#482878FF", "#3E4A89FF", "#31688EFF", "#26828EFF", "#1F9E89FF", "#35B779FF", "#6DCD59FF", "#B4DE2CFF", "#FDE725FF", "#FDE725FF", "#FDE725FF","#FDE725FF"),na.value = "black")+
     facet_wrap(~seizure, ncol = 1)+
     themeo + 
-    theme(
-      panel.background = element_rect(fill = 'black', colour = 'black')#,
-      #  plot.background = element_rect(fill = 'black', colour = 'black'),
-      # legend.text = element_blank()
-    )+
-    
-    #dbc195
-    #ffebba  565656
+    theme(panel.background = element_rect(fill = 'black', colour = 'black'))+
     
     coord_fixed(ylim = c(-45,55))+
     scale_x_continuous(expand = c(0,0))+
     scale_y_continuous(expand = c(0,0))+
-    
     xlab(NULL)+
     ylab(NULL)
   
   dens_maps  
+  
+  
+## load .csv file containing the curated fin modeling data
+## build plots of prob density of eez vs high seas
+## make a facet wrap for by 4 studies with loess
+## Becomes Figure 2b
+## requires some minor post processing on this facet in Ai & propoer dimension + sizing of PDF outputs  
+  fins3 <- read.csv('./data/fin_prob_eez_hiseas3.csv', header=T) ##; colnames(fins) = c('study', 'probability','count','region')
+  ggplot(data = fins3, aes(x=probability,  y=count, group=region, color=region)) +
+    theme(panel.grid = element_blank(),panel.background = element_blank()) +
+    scale_color_manual(values=c("#56B4E9", "#E69F00")) +
+    geom_point() +
+    geom_smooth(method = "loess", formula = y ~ x, span = 0.8, se = FALSE) +
+    scale_y_log10(limits = c(1,90000),
+                  breaks = trans_breaks("log10", function(x) 10^x),
+                  labels = trans_format("log10", math_format(10^.x))) +
+    annotation_logticks(sides="l") +
+    facet_wrap(~study, ncol=4)
+  
+## load .csv file ranking fin probs within eez
+## build bar plot of top 10 ranked countries by probability 
+## make facet of plots for 4 studies
+## requires some minor post processing on this facet in Ai & propoer dimension + sizing of PDF outputs  
+  
+  rank_eez_fins <- read.csv('./data/fin_eez_ranking.csv', header=T)
+  g <- rank_eez_fins %>%
+    ggplot(aes(rel_prob, rank)) +
+    theme_bw() +
+    scale_x_continuous(limits=c(0,1),breaks=c(0,0.2,0.4,0.6,0.8,1)) +
+    theme(panel.grid = element_blank(),panel.background = element_blank()) +
+    geom_col(mapping = NULL, data = NULL) +
+    scale_y_continuous(breaks = rank_eez_fins$rank, 
+                       labels = rank_eez_fins$eez, 
+                       expand = c(0,0))+
+    facet_wrap(~study, ncol = 4, scales ="free")
+  g 
+  
+  
+  
+## SEVERAL EXPLORATOTY PLOTS FOLLOW THAT WE DID NOUT USE
+## Simple histogram of probablity density by study
   
   stud_df %>% 
     #filter(NORMALIZED > 0) %>% 
@@ -101,7 +144,7 @@
     ylab(NULL)
   
   
-  # in EEZ vs overseas proportion
+## Prepping sourcing model outputs, segregating EEZ vs high seas 
   str(stud_df)
   
   #EEZ_df <- readOGR(dsn = "/Users/tgagne/shark_finning_2018/data/EEZ_land_union_v2_201410", layer = "EEZ_land_v2_201410") %>% tidy()
@@ -146,16 +189,16 @@
   
   str(sp_pts)
   
-  ## simple plot of sourcing model output
-  ## with points symboloized by high seas or EEZ
+## simple plot of sourcing model output
+## with points symboloized by high seas or EEZ
   ggplot(sp_pts)+
     geom_raster(aes(x=coords.x1, y = coords.x2, fill = in_eez))+
     facet_wrap(~seizure)+
     coord_fixed()
   
  
-  ## exploratory plot of Tyler's 
-  ## that never made it into the paper or OSM  
+## an exploratory data viz output 
+## this never made it into the paper or OSM  
   dens_fins <- sp_pts %>%  
     mutate(prob_cuts = as.numeric(cut_width(NORMALIZED,.009))) %>% 
     
@@ -188,8 +231,9 @@
     themeo
   
   
-  ## stack histogram comparing the EEZ vs high seas split probabilities
-  ## another summary plot of output of the sourcing model
+## another data viz output we never ended up using  
+## stack histogram comparing the EEZ vs high seas split probabilities
+## another summary plot of output of the sourcing model
   ggplot(sp_pts,aes( x = NORMALIZED)) + 
     geom_histogram(aes(fill = in_eez))+
     scale_fill_brewer(palette = "Dark2")+
